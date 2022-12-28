95d9d4fe520d4ad140cfc3feffb41453dafb66bf
Nehuen <villavicencionehuen@gmail.com>
2022-12-28 03:59:15 -0300

    v6

--- js/index.js
+++ js/index.js
@@ -5,16 +5,17 @@ window.sr = ScrollReveal();
             origin: 'top',
             distance: '400px'
         });
+        // sr.reveal('.section1txt', {
+        //     duration: 3000,
+        //     origin: 'left',
+        //     distance: '400px'
+        // });
         sr.reveal('.section1Container', {
             duration: 3000,
-            origin: 'right',
-            distance: '100px'
+            origin: 'bottom',
+            distance: '400px'
         });
-        sr.reveal('.navbar', {
-            duration: 3000,
-            origin: 'top',
-            distance: '100px'
-        });
+
         sr.reveal('.MPSTitle', {
             duration: 3000,
             origin: 'left',
@@ -49,16 +50,12 @@ window.sr = ScrollReveal();
             origin: 'right',
             distance: '400px'
         });
-        sr.reveal('.logoContainer', {
-            duration: 3000,
-            origin: 'left',
-            distance: '400px'
-        });
+
         sr.reveal('.socialMedia', {
-            duration: 1000,
+            duration: 3000,
 
         });
         sr.reveal('.credits', {
-            duration: 1000,
+            duration: 2000,
 
         });
