3e4093c3052852a5f17db7d6ae375f6862dbf6b5
Nehuen <villavicencionehuen@gmail.com>
2022-12-28 03:01:22 -0300

    v6

---
+++ js/index.js
@@ -0,0 +1,64 @@
+window.sr = ScrollReveal();
+
+        sr.reveal('.filterContainer', {
+            duration: 3000,
+            origin: 'top',
+            distance: '400px'
+        });
+        sr.reveal('.section1Container', {
+            duration: 3000,
+            origin: 'right',
+            distance: '100px'
+        });
+        sr.reveal('.navbar', {
+            duration: 3000,
+            origin: 'top',
+            distance: '100px'
+        });
+        sr.reveal('.MPSTitle', {
+            duration: 3000,
+            origin: 'left',
+            distance: '400px'
+        });
+        sr.reveal('.sneakersPopularContainer', {
+            duration: 3000,
+            origin: 'right',
+            distance: '400px'
+        });
+        sr.reveal('.section3', {
+            duration: 3000,
+
+        });
+        sr.reveal('.backgroundBenefit1', {
+            duration: 2000,
+            origin: 'left',
+            distance: '400px'
+        });
+        sr.reveal('.backgroundBenefit2', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '400px'
+        });
+        sr.reveal('.backgroundBenefit3', {
+            duration: 2000,
+            origin: 'right',
+            distance: '400px'
+        });
+        sr.reveal('.contentSuscribe', {
+            duration: 3000,
+            origin: 'right',
+            distance: '400px'
+        });
+        sr.reveal('.logoContainer', {
+            duration: 3000,
+            origin: 'left',
+            distance: '400px'
+        });
+        sr.reveal('.socialMedia', {
+            duration: 1000,
+
+        });
+        sr.reveal('.credits', {
+            duration: 1000,
+
+        });
